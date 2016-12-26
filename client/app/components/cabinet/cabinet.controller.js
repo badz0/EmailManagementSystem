4e1338adddb0b05cc34c803e3cdb2ca2e0175351
KostinStanislav <tigrarion@mail.ru>
2016-12-26 18:16:12 +0000

    fixed align

--- client/app/components/cabinet/cabinet.controller.js
+++ client/app/components/cabinet/cabinet.controller.js
@@ -2,14 +2,14 @@ import data from './cabinet.data.json';
 import countries from './cabinet.countries.json';
 class CabinetController {
   constructor() {}
-$onInit () {
+  $onInit () {
     this.name = 'cabinet';
     this.user = data;
     this.countries=countries;
-}
-submitForm(user) {
+  }
+  submitForm(user) {
       angular.copy(user, this.user[0]);
-      }
+  }
 }
 
 export default CabinetController;
