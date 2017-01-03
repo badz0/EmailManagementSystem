315632369c864819662a907b75f413d3e4f1e6a9
Bogdan Muranets <bogdan.muranets@gmail.com>
2017-01-03 17:47:37 +0200

    added translations for english and ukrainian

--- client/app/common/appheader/appheader.controller.js
+++ client/app/common/appheader/appheader.controller.js
@@ -1,7 +1,18 @@
 class AppheaderController {
+  constructor($translate) {
+    "ngInject";
+    this.lan = $translate.use();
+    this.changeLanguage = changeLanguage;
+
+    function changeLanguage(key) {
+      $translate.use(key);
+      this.lan = key;
+    };
+  };
+
   $onInit () {
     this.name = 'appheader';
-  }
+  };
 }
 
 export default AppheaderController;
