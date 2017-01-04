b0c66711d8c31eadcd0823bac492999fe7505693
KostinStanislav <tigrarion@mail.ru>
2017-01-04 12:45:34 +0000

    refresh submit form

--- client/app/components/cabinet/cabinet.controller.js
+++ client/app/components/cabinet/cabinet.controller.js
@@ -3,27 +3,32 @@ import * as firebase from 'firebase';
 
 class CabinetController {
   constructor($firebaseObject,Firedbservice) {'ngInject';
-    const ref = firebase.database().ref().child('user');
+    const ref = firebase.database().ref().child('user/9');
     this.users = $firebaseObject(ref);
   }
   $onInit () {
     this.name = 'cabinet';
     this.countries=countries;
   }
-  submitForm(user) {
-    user.country=user.country.country||'';
-    angular.extend(this.users[9],user);
-    firebase.database().ref().child('user/9').update(this.users[9]);
-    user.name='';
-    user.surname='';
-    user.login='';
-    user.city='';
-    user.postadress='';
-    user.birthDay='';
+  clearForm(){
+    this.user={
+      name:'',
+      surname:'',
+      login:'',
+      city:'',
+      postadress:'',
+      birthDay:'',
+      country:''
+    };
+  }
+  submitForm() {
+    this.user.country=this.user.country.country||'';
+    firebase.database().ref().child('user/9').update(this.user);
+    this.clearForm();
   }
   deleteAvatar(){
     firebase.storage().ref().child('user9/9.jpg').delete();
   }
 }
-
+  
 export default CabinetController;
