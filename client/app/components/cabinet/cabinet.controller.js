ba5e8b69cc2c0a905b5beff5b9bbb7ce28d62092
KostinStanislav <tigrarion@mail.ru>
2017-01-08 17:17:58 +0000

    uploading deleting and default

--- client/app/components/cabinet/cabinet.controller.js
+++ client/app/components/cabinet/cabinet.controller.js
@@ -5,10 +5,13 @@ class CabinetController {
   constructor($firebaseObject,Firedbservice) {'ngInject';
     const ref = firebase.database().ref().child('user/9');
     this.users = $firebaseObject(ref);
+    console.log(this.users);
+    
   }
   $onInit () {
     this.name = 'cabinet';
     this.countries=countries;
+    this.user={};
   }
   clearForm(){
     this.user={
@@ -27,7 +30,16 @@ class CabinetController {
     this.clearForm();
   }
   deleteAvatar(){
-    firebase.storage().ref().child('user9/9.jpg').delete();
+   let getFileName=()=>{ 
+      let url=this.users.avatar;
+      url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+      url = url.substring(url.lastIndexOf("%2F") + 3, url.length);
+      console.log(url);
+      return url;
+      };
+    firebase.storage().ref().child(`user9/${getFileName}`).delete();
+    this.user.avatar='https://firebasestorage.googleapis.com/v0/b/emailmanagementsystem-d4f11.appspot.com/o/user_avatar_default.gif?alt=media&token=188186f9-52fb-4557-a00e-7dd517e4e2d5';
+    firebase.database().ref().child('user/9').update(this.user);
   }
   clearCity(){
      this.user.city = null;
