21f7fcb3398fcf9025746b4a059437ea3aa6e6f2
Bogdan Muranets <bogdan.muranets@gmail.com>
2017-01-09 17:40:08 +0200

    created component for language buttons

--- client/app/common/appnav/appnav.controller.js
+++ client/app/common/appnav/appnav.controller.js
@@ -1,16 +1,6 @@
 class AppnavController {
-  constructor($translate) {
-    'ngInject';
-    this.translate = $translate;
-  }
-
   $onInit() {
-    this.lan = this.translate.use();
-  }
-
-  changeLanguage(key) {
-    this.translate.use(key);
-    this.lan = key;
+    this.name = 'appnav';
   }
 }
 
