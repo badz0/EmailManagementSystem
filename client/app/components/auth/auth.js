25ff3950639798a768f4baaccf45b2ff850186e0
KostinStanislav <tigrarion@mail.ru>
2017-01-17 09:34:05 +0000

    rebases

--- client/app/components/auth/auth.js
+++ client/app/components/auth/auth.js
@@ -6,10 +6,7 @@ import AuthRun from './auth.run';
 import 'auth0-lock';
 import 'angular-lock';
 import 'angular-jwt';
-<<<<<<< HEAD
-=======
 import Firedbservice from '../../firedb.service.js';
->>>>>>> develop
 
 let authModule = angular.module('auth', [
   uiRouter,
@@ -29,27 +26,17 @@ let authModule = angular.module('auth', [
     clientID: 'YWiJP0aecm768DSElJl8YhqtIbAgx7gm',
     domain: 'nerosman.eu.auth0.com'
   });
-<<<<<<< HEAD
     
   jwtOptionsProvider.config({
     tokenGetter: function () {
       return window.localStorage.getItem('id_token');
-=======
-
-  jwtOptionsProvider.config({
-    tokenGetter: function () {
-      return localStorage.getItem('id_token');
->>>>>>> develop
     }
   });
   $urlRouterProvider.otherwise('/auth');
 })
 
 .component('auth', authComponent)
-<<<<<<< HEAD
-=======
 .service('Firedbservice',Firedbservice)
->>>>>>> develop
 .service('AuthService', AuthService)
 .run(()=>AuthRun)
 .name;
