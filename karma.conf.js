c04dffdc3f84ba6e53519b515dc74bc79332dbcc
Nadolskyi <nadolskyy@i.ua>
2017-01-31 08:42:54 +0000

    fixed_tests

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { 'spec.bundle.js': ['webpack', 'sourcemap', 'coverage'] 
+    preprocessors: { 'spec.bundle.js': ['webpack', 'sourcemap'] 
     },
     devtool: 'inline-source-map',
      webpack: {
@@ -56,8 +56,8 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-     coverageReporter: {
+    reporters: ['mocha','progress', 'coverage'],
+    coverageReporter: {
       type : 'html',
       dir : 'coverage/'
     },
