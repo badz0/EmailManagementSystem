8948045ddf8627f1cc0fca690b873f357f493589
Nerosman <Nerosman2601@gmail.com>
2017-01-31 22:07:10 +0200

    add more test

--- client/app/components/auth/auth.spec.js
+++ client/app/components/auth/auth.spec.js
@@ -16,12 +16,21 @@ describe('Auth', () => {
     });
   });
 
+  describe('Controller', () => {
+    let controller = AuthController;
+    it('should have a name property', ()=>{
+      expect(controller.name).toEqual('AuthController'); 
+    });
+  });
+
   describe('Service', () => {
-    let aService, lock, authManager, FiredbAutorisation;
+    let aService, lock, authManager, FiredbAutorisation, localStorage, lockProvider;
     beforeEach(inject(($q, $state) => {
-      lock = jasmine.createSpyObj('lock', ['on', 'show', 'getUserInfo']);
-      authManager = jasmine.createSpyObj('authManager', ['unauthenticate', 'authenticate', 'isAuthenticated']);
+      lock = jasmine.createSpyObj('lock', ['on', 'show', 'getUserInfo', 'interceptHash']);
+      authManager = jasmine.createSpyObj('authManager', ['unauthenticate', 'authenticate', 'isAuthenticated', 'checkAuthOnRefresh']);
       FiredbAutorisation = jasmine.createSpyObj('FiredbAutorisation', ['getUserDetailsArr']);
+      localStorage = jasmine.createSpyObj('localStorage', ['removeItem', 'setItem']);
+      lockProvider = jasmine.createSpyObj('lockProvider', ['init']);
       aService = new AuthService($q, lock, authManager, FiredbAutorisation, $state);
     }));
     it('have 5 function', () => {
@@ -34,17 +43,20 @@ describe('Auth', () => {
     it('Сheck if FiredbAutorisation was called', () => {
       expect(FiredbAutorisation.getUserDetailsArr).toHaveBeenCalled();
     });
-    it('Can show plugin', () => {
-      expect(lock.show).toBeDefined();
-    });
     it('Can get clientID and domain from Auth0 client', ()=> {
       expect(Auth0Lock).toBeDefined();
+    });
+    it('Can show plugin', () => {
+      expect(lock.show).toBeDefined();
     });
     it('Can authenficated', () => {
       expect(lock.on).toBeDefined();
     });
     it('Can get users info', () => {
       expect(lock.getUserInfo).toBeDefined();
+    });
+    it('Run function lock.interceptHash', () => {
+      expect(lock.interceptHash).toBeDefined();
     });
     it('isAuthenticated = true', () => {
       authManager.isAuthenticated=true;
@@ -56,6 +68,19 @@ describe('Auth', () => {
     });
     it('isUnAuthenticated?', () => {
       expect(authManager.unauthenticate).toBeDefined();
+    });
+    it('have Authenticated function?', () => {
+      expect(authManager.authenticate).toBeDefined();
+    });
+    it('can handle session on refresh', () => {
+      expect(authManager.checkAuthOnRefresh).toBeDefined();
+    });
+    it('can work with local storage', () => {
+      expect(localStorage.removeItem).toBeDefined();
+      expect(localStorage.setItem).toBeDefined();
+    });
+    it('can init', () => {
+      expect(lockProvider.init).toBeDefined();
     });
   });
 });
