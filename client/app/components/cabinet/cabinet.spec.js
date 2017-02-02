e4ece51b62a16256c2564b5de97af1d7511ed55f
KostinStanislav <tigrarion@mail.ru>
2017-02-02 08:42:40 +0000

    layout3

--- client/app/components/cabinet/cabinet.spec.js
+++ client/app/components/cabinet/cabinet.spec.js
@@ -11,112 +11,105 @@ describe('Cabinet', ()=>{
     let scope, controller,$firebaseObject,mdColorPalette,Сonstants;
     let FiredbAutorisation = {};
     beforeEach(inject(($injector, $controller, $q ) => {
-    	FiredbAutorisation.responseData = () => {};
-    	FiredbAutorisation.getUserData = () => {};
-    	FiredbAutorisation.updateUser = () => {};
-    	FiredbAutorisation.deleteUserAvatar = () => {};
-    	mdColorPalette={'green':500};
-    	Сonstants = cabinetConstant;
-    	spyOn(FiredbAutorisation, 'responseData').and.callFake( () => {
-    		let defer = $q.defer();
-    		defer.resolve({ userData: {index:9}});
-    		return defer.promise;
-    	});
-    	spyOn(FiredbAutorisation,'getUserData').and.callFake( () => {
-    		return {"avatar" : "https://firebasestorage.googleapis.com/v0/b/emailmanagementsystem-d4f11.appspot.com/o/user9%2F9.png?alt=media&token=cb537f71-6f8d-4300-b9f3-5f8b3c8d37b0"};
-    	});
-    	spyOn(FiredbAutorisation,'updateUser').and.callFake( () => {
-    		return "executed"; 
-    	});
-    	spyOn(FiredbAutorisation,'deleteUserAvatar').and.callFake( () => {
-    		return "executed"; 
-    	});
-    	scope = $injector.get('$rootScope').$new();
-    	controller = $controller(CabinetController, {
-    		$firebaseObject: $firebaseObject,
-    		$scope: scope,
-    		FiredbAutorisation: FiredbAutorisation,
-    		$mdColorPalette:mdColorPalette,
-    		Сonstants:Сonstants
-    	});
-    	scope.$digest();
-    }));
-    
-	it('constructor shoud be defined', ()=>{
-		expect(controller.constructor).toBeDefined();
-	});
-	it('should get object with users data', ()=>{
-		expect(controller.users).toEqual(jasmine.any(Object));
-	});
-	it('all methods should be defined', ()=>{
-		expect(controller.$onInit).toBeDefined();
-		expect(controller.clearCity).toBeDefined();
-		expect(controller.submitForm).toBeDefined();
-		expect(controller.getFileName).toBeDefined();
-		expect(controller.deleteAvatar).toBeDefined();
-		expect(controller.selectTheme).toBeDefined();
-	});
-	it('should have a name property', ()=>{
-		let controller = CabinetController;
-		expect(controller.name).toEqual('CabinetController'); 
-	});
-	
-	describe('$onInit()',()=>{
-		it('countries should be object',()=>{
-			controller.$onInit();
-			expect(controller.countries).toEqual(countries);
-		});
-		it('user should be object',()=>{
-			controller.$onInit();
-			expect(controller.user).toEqual(jasmine.any(Object));
-		});
-	});
-	
-	describe('clearCity()',()=>{
-		it('called and executed properly',()=>{
-		controller.$onInit();
-		controller.clearCity();
-		expect(controller.user.city).toEqual(null);
-		});
-	});
-	
-	describe('deleteAvatar()',()=>{
-		it('called and executed properly',()=>{
-		controller.$onInit();
-		controller.deleteAvatar();
-		expect(controller.FiredbAutorisation.deleteUserAvatar).toHaveBeenCalled();
-		expect(controller.FiredbAutorisation.updateUser).toHaveBeenCalled();
-		});
-	});
-	
-	describe('getFileName()',()=>{
-		it('called and executed properly ', () => {
-		controller.$onInit();
-		expect(controller.getFileName()).toEqual('9.png')
+      FiredbAutorisation.responseData = () => {};
+      FiredbAutorisation.getUserData = () => {};
+      FiredbAutorisation.updateUser = () => {};
+      FiredbAutorisation.deleteUserAvatar = () => {};
+      mdColorPalette={'green':500};
+      Сonstants = cabinetConstant;
+      spyOn(FiredbAutorisation, 'responseData').and.callFake( () => {
+        let defer = $q.defer();
+        defer.resolve({ userData: {index:9}});
+        return defer.promise;
       });
-	});
-		
-	describe('submitForm()',()=>{
-		it('called and executed properly',()=>{
-		controller.$onInit();
-		controller.user.country={'country':"USA"};
-		controller.submitForm();
-		expect(controller.FiredbAutorisation.updateUser).toHaveBeenCalled();
-		expect(controller.user).toEqual(jasmine.any(Object));
-		});
-	});
-		
-	describe('selectTheme(color)',()=>{
-		it('called and executed properly',()=>{
-		controller.$onInit();
-		let color='green';
-		controller.selectTheme(color);
-		expect(controller.user.themeColor).toEqual(jasmine.any(String));
-		expect(controller.FiredbAutorisation.updateUser).toHaveBeenCalled();
-		});
-	});
+      spyOn(FiredbAutorisation,'getUserData').and.callFake( () => {
+        return {"avatar" : "https://firebasestorage.googleapis.com/v0/b/emailmanagementsystem-d4f11.appspot.com/o/user9%2F9.png?alt=media&token=cb537f71-6f8d-4300-b9f3-5f8b3c8d37b0"};
+      });
+      spyOn(FiredbAutorisation,'updateUser').and.callFake( () => {
+        return "executed"; 
+      });
+      spyOn(FiredbAutorisation,'deleteUserAvatar').and.callFake( () => {
+        return "executed"; 
+      });
+      scope = $injector.get('$rootScope').$new();
+      controller = $controller(CabinetController, {
+        $firebaseObject: $firebaseObject,
+        $scope: scope,
+        FiredbAutorisation: FiredbAutorisation,
+        $mdColorPalette:mdColorPalette,
+        Сonstants:Сonstants
+      });
+      scope.$digest();
+    }));
+    it('constructor shoud be defined', ()=>{
+      expect(controller.constructor).toBeDefined();
+    });
+    it('should get object with users data', ()=>{
+      expect(controller.users).toEqual(jasmine.any(Object));
+    });
+    it('all methods should be defined', ()=>{
+      expect(controller.$onInit).toBeDefined();
+      expect(controller.clearCity).toBeDefined();
+      expect(controller.submitForm).toBeDefined();
+      expect(controller.getFileName).toBeDefined();
+      expect(controller.deleteAvatar).toBeDefined();
+      expect(controller.selectTheme).toBeDefined();
+    });
+    it('should have a name property', ()=>{
+      let controller = CabinetController;
+      expect(controller.name).toEqual('CabinetController'); 
+    });
+    describe('$onInit()',()=>{
+      it('countries should be object',()=>{
+        controller.$onInit();
+        expect(controller.countries).toEqual(countries);
+      });
+      it('user should be object',()=>{
+        controller.$onInit();
+        expect(controller.user).toEqual(jasmine.any(Object));
+      });
+    });
+    describe('clearCity()',()=>{
+      it('called and executed properly',()=>{
+        controller.$onInit();
+        controller.clearCity();
+        expect(controller.user.city).toEqual(null);
+      });
+    });
+    describe('deleteAvatar()',()=>{
+      it('called and executed properly',()=>{
+        controller.$onInit();
+        controller.deleteAvatar();
+        expect(controller.FiredbAutorisation.deleteUserAvatar).toHaveBeenCalled();
+        expect(controller.FiredbAutorisation.updateUser).toHaveBeenCalled();
+      });
+    });
+    describe('getFileName()',()=>{
+      it('called and executed properly ', () => {
+        controller.$onInit();
+        expect(controller.getFileName()).toEqual('9.png')
+      });
+    });
+    describe('submitForm()',()=>{
+      it('called and executed properly',()=>{
+        controller.$onInit();
+        controller.user.country={'country':"USA"};
+        controller.submitForm();
+        expect(controller.FiredbAutorisation.updateUser).toHaveBeenCalled();
+        expect(controller.user).toEqual(jasmine.any(Object));
+      });
+    });
+    describe('selectTheme(color)',()=>{
+      it('called and executed properly',()=>{
+        controller.$onInit();
+        let color='green';
+        controller.selectTheme(color);
+        expect(controller.user.themeColor).toEqual(jasmine.any(String));
+        expect(controller.FiredbAutorisation.updateUser).toHaveBeenCalled();
+      });
+    });
   });
-	
+  
   describe('Component', () => {
     let component = CabinetComponent;
     it('includes the intended template',() => {
@@ -126,7 +119,6 @@ describe('Cabinet', ()=>{
       expect(component.controller).toEqual(CabinetController);
     });
   });
-  
   describe('Constant', () => {
     let image=cabinetConstant.avatarDefault.IMAGE_LINK;
     it('should use correct default link and to be a string',() => {
@@ -138,9 +130,9 @@ describe('Cabinet', ()=>{
   describe('Directive', () => {
     let directive=new cabinetDirective();
     it('should have correct properties',() => {
-		expect(directive.controller).toEqual(jasmine.any(Function));
-		expect(directive.restrict).toBe('A');
-		expect(directive.bindToController).toBeTruthy();
+      expect(directive.controller).toEqual(jasmine.any(Function));
+      expect(directive.restrict).toBe('A');
+      expect(directive.bindToController).toBeTruthy();
     });
     it('link should be function',() => {
       expect(directive.link).toEqual(jasmine.any(Function));
